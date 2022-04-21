72cbdd858a33fb79bcd7bcb13a1ac06ad9388847
Hannah Senft <hannah.marie.senft@gmail.com>
2022-04-21 12:41:30 -0400

    link id to product page and working on content add

--- front/js/product.js
+++ front/js/product.js
@@ -1,8 +1,28 @@
 const idProduct = new URL(window.location.href).searchParams.get("id");
 console.log(idProduct)
 
-const items = document.getElementById('items');
+fetch('http://localhost:3000/api/products/' + idProduct)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+    makeProductCard(data);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+  
+const productObject = {
+    color: '',
+    id: '',
+    name: '',
+    imgUrl: '',
+    price: '',
+    qty: 1
+}
 
+function makeProductCard(obj) {
 // updating quantity
 const quantity = document.getElementById('quantity');
 quantity.addEventListener('change', quantity);
@@ -10,3 +30,14 @@ quantity.addEventListener('change', quantity);
 // add to cart
 const addBtn = document.getElementById('addToCart');
 addToCart.addEventListener('click', addToCart);
+
+// color change pulldown
+makePullDown(obj.colors);
+}
+
+// pulldown card function
+function makePullDown(arr) {
+    const pullDown = document.getElementById('colors');
+    pullDown.addEventListener('change', updateColor);
+
+  }
