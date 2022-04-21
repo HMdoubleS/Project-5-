0eb4e8e746b6dfeab5d485b58d2652a8ea4dad58
Hannah Senft <hannah.marie.senft@gmail.com>
2022-04-21 12:11:13 -0400

    quantity and  cart event listeners

--- front/js/product.js
+++ front/js/product.js
@@ -3,3 +3,10 @@ console.log(idProduct)
 
 const items = document.getElementById('items');
 
+// updating quantity
+const quantity = document.getElementById('quantity');
+quantity.addEventListener('change', quantity);
+
+// add to cart
+const addBtn = document.getElementById('addToCart');
+addToCart.addEventListener('click', addToCart);
