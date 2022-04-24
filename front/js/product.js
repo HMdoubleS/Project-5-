841a95dc14ef9d407a592e9b42272c578f7bd807
Hannah Senft <hannah.marie.senft@gmail.com>
2022-04-24 13:28:41 -0400

    adding  contents to page

--- front/js/product.js
+++ front/js/product.js
@@ -5,20 +5,40 @@ console.log(idProduct)
 fetch('http://localhost:3000/api/products/' + idProduct)
     .then(res => res.json())
     .then(data => {
+        console.log(data);
     makeProductCard(data);
     })
     .catch(err => console.log(err));
   
 
 function makeProductCard(obj) {
-    
+// variables for DOM elements
+const prodImg = document.querySelector('.item__img');
+const prodDescription = document.getElementById('description'); 
+const prodPrice = document.getElementById('price');
+const prodTitle = document.getElementById('title');
+const prodQuantity = document.getElementById('quantity');
+const addBtn = document.getElementById('addToCart');
+const pullDown = document.getElementById('colors');
+
+// add content to elements
+
+//image
+const itemImg = document.createElement('img');
+itemImg.setAttribute('src', obj.imageUrl);
+prodImg.appendChild(itemImg);
+
+//price, title and description
+prodPrice.innerHTML = obj.price;
+prodTitle.innerHTML = obj.name;
+prodDescription.innerHTML = obj.description;
+
+
 // updating quantity
-const quantity = document.getElementById('quantity');
 quantity.addEventListener('change', quantity);
 
 // add to cart
-const addBtn = document.getElementById('addToCart');
-addToCart.addEventListener('click', addToCart);
+addBtn.addEventListener('click', addToCart);
 
 // color change pulldown
 makePullDown(obj.colors);
@@ -26,18 +46,9 @@ makePullDown(obj.colors);
 
 // pulldown card function
 function makePullDown(array) {
-    const pullDown = document.getElementById('colors');
     pullDown.addEventListener('change', colors);
     //needs to loop through color options but not sure how to get the color value
   }
-
-
-let productImg = document.getElementById('imageUrl'); 
-let productName = document.getElementById('name');
-let productPrice = document.getElementById('price');
-let productDescription = document.getElementById('description');
-
-
 
 //how do I get the img, price, description onto the page and alter them with the id?
 //need to link the cart button to the cart page
